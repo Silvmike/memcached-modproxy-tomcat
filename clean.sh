8821771dc22a8287c7af9ae455c34ba5fab5ffcb
Michael Silvanovich <silvanovich.michael@gmail.com>
2016-06-05 16:46:25 +0300

    fixed scripts

--- clean.sh
+++ clean.sh
@@ -2,5 +2,6 @@
 cd memcached-manager-improved
 mvn clean
 rm -f ../tomcat/tomcat/lib/mmi.jar
+cd ../simple-web-app
 mvn clean
 rm -f ../tomcat/tomcat/webapps/simple.war
--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+mkdir -p /tmp/tomcat7.0.69
+wget -O /tmp/tomcat7.0.69/tomcat.tar.gz http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz
+tar -xvzf /tmp/tomcat7.0.69/tomcat.tar.gz --keep-old-files --strip-components=1 --directory ./tomcat/tomcat
 cd memcached-manager-improved
 mvn clean compile assembly:single
 cp -f target/mmi-jar-with-dependencies.jar ../tomcat/tomcat/lib/mmi.jar
 cd ../simple-web-app
 mvn clean package
 cp -f target/simple.war ../tomcat/tomcat/webapps/simple.war
+rm -rf /tmp/tomcat7.0.69
